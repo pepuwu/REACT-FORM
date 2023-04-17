11fb9eb6e39b929b7b5c58315d8bd670c64a24a0
PEDRO ESTEBAN <pesteban@andreani.com>
2023-04-17 13:08:42 -0300

    feat: implementación de card empleado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Grid } from '@mui/material'
 import './App.css'
 import Formulario from './components/form/'
+import SearchEmployed, { CardEmployed } from './components/employed'
+import { useState } from 'react'
 
-function App () {
+const App = () => {
+  const [listaEmpleados, setListaEmpleados] = useState([])
   return (
-    <Formulario />
+    <div className='wallpaper'>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Formulario guardarEmpleado={setListaEmpleados} props={listaEmpleados} />
+        </Grid>
+        <Grid item xs={6}>
+          <SearchEmployed />
+          <CardEmployed listaEmpleados={listaEmpleados} />
+        </Grid>
+      </Grid>
+
+    </div>
   )
 }
-
 export default App
